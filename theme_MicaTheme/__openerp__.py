4af0e12dfb0ae77d84c3175204ddae976dc8d577
Micael <micael11rm@ourlook.com>
2016-01-29 14:01:02 +0100

    HACEMOS UN COMMIT PARA GUBEN

---
+++ theme_MicaTheme/__openerp__.py
@@ -0,0 +1,12 @@
+{
+  'name':'MicasTheme',
+  'description': 'theme from guben',
+  'version':'1.0',
+  'author':'Micael',
+
+  'data': [
+  ],
+  'category': 'Theme/Entertainment',
+  'depends': ['website', 'website_blog', 'sale'],
+
+}
